ecde4861842dbb234120ffd859f6a2479acab16a
Odontopediatra <entutarget+odontopediatra@gmail.com>
2023-07-01 11:38:39 -0600

    Cambio en astro config línea 8

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://odontopediatra.github.io',
+  site: 'https://Odontopediatra.github.io',
   base: '/odontopediatra-doctora-andrea-flores/',
 });
